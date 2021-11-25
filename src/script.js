c8b93b94b27348e249c50fe3ed6d400411dccf84
tracksuite <youngunner1998@gmail.com>
2021-11-25 16:26:54 +0530

    developed a method for pixel reduction

--- src/script.js
+++ src/script.js
@@ -23,9 +23,24 @@ scene.add(mesh)
  * Sizes
  */
 const sizes = {
-    width: 800,
-    height: 600
+    width: window.innerWidth,
+    height: window.innerHeight
 }
+//to customize when it resizing
+window.addEventListener('resize',() =>
+{
+    //update sizes
+    sizes.width = window.innerWidth
+    sizes.height = window.innerHeight
+
+    //update camera
+    camera.aspect = sizes.width / sizes.height
+    camera.updateProjectionMatrix()
+
+    //update renderer
+    renderer.setSize(sizes.width, sizes.height)
+
+})
 
 /**
  * Camera
@@ -46,6 +61,7 @@ const renderer = new THREE.WebGLRenderer({
     canvas: canvas
 })
 renderer.setSize(sizes.width, sizes.height)
+renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
 
 /**
  * Animate
